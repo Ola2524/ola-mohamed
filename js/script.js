fa991a59950d24f5e0526158101d5bf868058bdf
Ola2524 <o.mohamed.mmb@gmail.com>
2023-05-30 20:41:42 +0300

    responsive and add about section

--- js/script.js
+++ js/script.js
@@ -91,6 +91,17 @@ $(document).ready(function(){
         items: 3,
         margin: 20,
         loop: true,
-        autoplay: true
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
     });
 });
